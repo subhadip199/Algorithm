677b9f633436d152dbee719a016548a0a305fd11
subhadip nandi <48771530+subhadip199@users.noreply.github.com>
2020-04-21 20:09:41 +0530

    Add files via upload
    iterative

---
+++ Dynamic problem/Longest Common Subsequence(iterative)/Ide.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Main
+{
+    public static int LCS(char x[],char y[],int n,int m )
+    {
+        int[][] t= new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            
+            for(int j=0;j<=m;j++){
+                
+            if(i==0|| j==0)
+            {
+                t[i][j]= 0;
+            }
+            else if(x[i-1]==y[j-1])
+            {
+               t[i][j]= 1+t[i-1][j-1];
+            }
+            else{
+                t[i][j]= Math.max(t[i-1][j],t[i][j-1]);
+            }
+          }
+        }
+        return t[n][m];
+    }
+	public static void main(String[] args) {
+	
+	  Scanner sc=new Scanner(System.in);
+	  String s1= sc.next();
+	  String s2= sc.next();
+	  char[] ch1=s1.toCharArray();
+	  char[] ch2=s2.toCharArray();
+	  System.out.println(LCS(ch1,ch2,ch1.length,ch2.length));
+	}
+}
