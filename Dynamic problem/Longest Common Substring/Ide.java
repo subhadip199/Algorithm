9fa1d4dac3fbeadc70acaabd3fa2cdf93e6eb8e6
subhadip nandi <48771530+subhadip199@users.noreply.github.com>
2020-04-22 00:53:29 +0530

    Add files via upload

---
+++ Dynamic problem/Longest Common Substring/Ide.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Main
+{
+    public static int LCS(char x[],char y[],int n,int m )
+    {
+        int[][] t= new int[n+1][m+1];
+        int result=0;
+        for(int i=0;i<=n;i++){
+            
+            for(int j=0;j<=m;j++){
+                
+            if(i==0|| j==0)
+            {
+                t[i][j]= 0;
+            }
+            else if(x[i-1]==y[j-1])
+            {
+               t[i][j]= 1+t[i-1][j-1];
+               result = Math.max(result,t[i][j]); 
+            }
+            else{
+                t[i][j]= 0;
+            }
+          }
+        }
+        return result;
+    }
+	public static void main(String[] args) {
+	
+	  Scanner sc=new Scanner(System.in);
+	  String s1= sc.next();
+	  String s2= sc.next();
+	  char[] ch1=s1.toCharArray();
+	  char[] ch2=s2.toCharArray();
+	  System.out.println(LCS(ch1,ch2,ch1.length,ch2.length));
+	}
+}
